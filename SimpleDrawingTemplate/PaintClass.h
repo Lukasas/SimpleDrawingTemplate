ac7706e18fcf83448c38b0e38b83ad59aacf1e39
Lukáš Chábek <lukaschabek@gmail.com>
2018-08-16 15:09:07 +0200

    Added DirectX support
    GDI and GDI+ were disabled due to DirectX development.
    Pixels are drawn as rectangles with size 1 - not accurate.

--- SimpleDrawingTemplate/PaintClass.h
+++ SimpleDrawingTemplate/PaintClass.h
@@ -1,25 +1,31 @@
 #pragma once
 //#define GDI_PLUS_PAINT
-#ifdef GDI_PLUS_PAINT
+//#define DIRECTX
+//#ifdef GDI_PLUS_PAINT
+//
+//#include "GdiPlusPaint.h"
+//class PaintClass :
+//	public GdiPlusPaint
+//#elseif DIRECTX
+//#else
+//#include "GdiPaint.h"
+//class PaintClass :
+//	public GdiPaint
+//#endif // GDI_PLUS_PAINT
 
-#include "GdiPlusPaint.h"
-class PaintClass :
-	public GdiPlusPaint
-
-#else
-#include "GdiPaint.h"
-class PaintClass :
-	public GdiPaint
-#endif // GDI_PLUS_PAINT
+#include "Direct2DPaint.h"
+	class PaintClass :
+	public Direct2DPaint
 {
 public:
 	PaintClass(HWND hWnd) : 
-
-#ifdef GDI_PLUS_PAINT
-		GdiPlusPaint(hWnd)
-#else
-		GdiPaint(hWnd)
-#endif // GDI_PLUS_PAINT
+//
+//#ifdef GDI_PLUS_PAINT
+//		GdiPlusPaint(hWnd)
+//#else
+//		GdiPaint(hWnd)
+//#endif // GDI_PLUS_PAINT
+	Direct2DPaint(hWnd)
 	{
 		SetDrawingMode(Fullscreen);
 		Setup();
